86a159ae401f8d320642d8fed49fe7585192020d
waqqasazim <waqqasazim@gmail.com>
2025-02-24 15:12:11 +0530

    Add files via upload

---
+++ include/head.php
@@ -0,0 +1,16 @@
+<!-- libraries CSS -->
+<link rel="stylesheet" href="assets/icon/flaticon_real_estate.css">
+<link rel="stylesheet" href="assets/vendor/bootstrap/bootstrap.min.css">
+<link rel="stylesheet" href="assets/vendor/splide/splide.min.css">
+<link rel="stylesheet" href="assets/vendor/swiper/swiper-bundle.min.css">
+<link rel="stylesheet" href="assets/vendor/slim-select/slimselect.css">
+<link rel="stylesheet" href="assets/vendor/animate-wow/animate.min.css">
+
+<!-- custom CSS -->
+<link rel="stylesheet" href="assets/css/style.css">
+
+<!-- favicon -->
+<link rel="icon" href="assets/img/favicon.icon" sizes="32x32" />
+<link rel="icon" href="assets/img/favicon.png" sizes="32x32" />
+<!-- <link rel="icon" href="assets/img/logo-32x32.png" sizes="32x32" />
+<link rel="icon" href="assets/img/logo-192x192.png" sizes="192x192" /> -->
---
+++ include/header.php
@@ -0,0 +1,74 @@
+ <!-- HEADER SECTION START -->
+ <header class="ul-header ul-header--home">
+        <div class="ul-header-bottom">
+            <div class="ul-header-bottom-wrapper">
+                <!-- header left -->
+                <div class="header-bottom-left">
+                    <div class="logo-container">
+                        <a href="index.php" class="d-inline-block"><img src="assets/img/logo.png" alt="logo" class="logo"></a>
+                    </div>
+                </div>
+
+                <!-- header nav -->
+                <div class="ul-header-nav-wrapper">
+                    <div class="to-go-to-sidebar-in-mobile">
+                        <nav class="ul-header-nav">
+                            <a class="black-a" href="index.php">Home</a>
+                            <a class="black-a" href="about.php">About</a>
+                            <div class="has-sub-menu">
+                                <a class="black-a" role="button">Property</a>
+
+                                <div class="ul-header-submenu">
+                                    <ul>
+                                        <li><a href="projects.php">Properties</a></li>
+                                        <li><a href="project-details.php">Property Details</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="has-sub-menu">
+                                <a class="black-a" role="button">Pages</a>
+
+                                <div class="ul-header-submenu">
+                                    <ul>
+                                        <li><a href="services.php">Services</a></li>
+                                        <li><a href="service-details.php">Service Details</a></li>
+                                        <li><a href="team.php">Team</a></li>
+                                        <li><a href="team-details.php">Team Details</a></li>
+                                        <li><a href="how-it-works.php">How it works Page</a></li>
+                                        <li><a href="pricing.php">Pricing Page</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="has-sub-menu">
+                                <a class="black-a" role="button">Blog</a>
+
+                                <div class="ul-header-submenu">
+                                    <ul>
+                                        <li><a href="blog.php">Blogs</a></li>
+                                        <li><a href="blog-2.php">Blog layout 2</a></li>
+                                        <li><a href="blog-details.php">Blog Details</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <a class="black-a" href="contact.php">Contact</a>
+                        </nav>
+                    </div>
+                </div>
+
+                <!-- actions -->
+                <div class="ul-header-actions">
+                    <!-- <button class="ul-header-search-opener"><i class="flaticon-search"></i></button> -->
+                    <!-- <button><i class="flaticon-user"></i></button> -->
+                    <a href="tel:+919953035559" class="add-property-btn d-xxs-none"><i class="flaticon-call"></i> +91-9953035559</a>
+                    <button class="ul-header-sidebar-opener"><i class="flaticon-menu-button"></i></button>
+                </div>
+
+                <!-- sidebar opener -->
+                <div class="d-none">
+                    <button class="ul-header-sidebar-opener"><i class="flaticon-menu-button"></i></button>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- HEADER SECTION END -->
